419a9d846022cd536573826df70f493729596f43
HienHy <hiennguyenhy0911@gmail.com>
2022-03-15 00:05:59 +0700

    Update baitaap1_2.c

--- baitaap1_2.c
+++ baitaap1_2.c
@@ -2,10 +2,10 @@
  
  #include<stdio.h>
 int main(){
-	float dola;
+	float dola, giatri;
 	int vnd = 23000;
 	printf("enter dolla =");
 	scanf("%f", &dola);
-    vnd = dola * vnd;
-	printf(" so vnd = %d", vnd);
+    giatri = dola * vnd;
+	printf(" so vnd = %.2f", giatri);
 	}
